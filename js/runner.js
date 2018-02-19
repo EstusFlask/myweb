8bc95e9f3f0434dfb2465c87ebb07743902abc6f
Toni <antoniorodriguezvega@gmail.com>
2018-02-19 13:45:54 +0100

    Mejorado el menú y añadidas redes sociales

--- js/runner.js
+++ js/runner.js
@@ -3,19 +3,17 @@ var flipflap = function(){
 	$('#hablaconmigo').css('position','relative');
 	$('#hablaconmigo')
 	.animate({
-		opacity: '0.5',
-		top: '10px'
+		opacity: '0.5'
 	},'slow')
 	.animate({
-		opacity:'1',
-		top: '-10px'
+		opacity:'1'
 	}, 'slow');
 		setTimeout(flipflap());
 	}
 
 $(function(){
 	//Animación icono menú.
-	$('.animated-icon3').click(function(){
+	$('.animated-icon1').click(function(){
         $(this).toggleClass('open');
   	});
   	//Animación botón flotando.
