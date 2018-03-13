af4026c434c29447e946daa1cee52b4050ad30af
Toni <antoniorodriguezvega@gmail.com>
2018-03-13 18:46:50 +0100

    corregidos colores

--- js/runner.js
+++ js/runner.js
@@ -1,11 +1,27 @@
 var flagLogo = true;
+
+var ajaxContent = function(doc){
+	var jqXHR = $.ajax({
+		url:'loads/'+doc,
+		async:true,
+		type:'get'
+	});
+
+	jqXHR.done(function(doc){
+		$('#content').html('').append(doc);
+	});
+
+};
+
 var myLogoSize = function(){
 	if (flagLogo==true) {
 		$('#mylogo').animate({ width: '35px'},'slow');
+		$("#content").animate({marginTop: "+=100px"});
 				flagLogo=false;	
 				}
 	 else {
 		$('#mylogo').animate({width: '50px'},'slow');
+		 $("#content").animate({marginTop: "-=100px"});
 				flagLogo=true;
 		}
 	}
@@ -18,7 +34,11 @@ $(function(){
         myLogoSize();
   	});
   	
+	ajaxContent('home.html');
 
+	$('#home').click(function(){
+		ajaxContent('home.html');
+	})
 	
 });
 
